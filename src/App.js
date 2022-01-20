01933eec90b44379490514bbc2a2d50aec983418
Antony Quintero <antonyquinterog@gmail.com>
2022-01-20 17:37:55 -0600

    Adding Preview Collection and styles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import "./App.css";
 import HomePage from "./page/homepage/homepage.component.jsx";
+import ShopPage from "./page/shop/shop.component.jsx";
 import { Switch, Route } from "react-router-dom";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
